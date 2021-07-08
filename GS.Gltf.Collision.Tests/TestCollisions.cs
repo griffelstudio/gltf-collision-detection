69318785e9494b4c854edf9b8621aee867d5df78
Alexanderexe <cronergy100@mail.ru>
2021-07-09 00:49:04 +0300

    modify tests for new models

--- GS.Gltf.Collision.Tests/TestCollisions.cs
+++ GS.Gltf.Collision.Tests/TestCollisions.cs
@@ -7,6 +7,7 @@ namespace GS.Gltf.Collision.Tests
 {
     public class TestCollisions
     {
+        private readonly string path = typeof(TestCollisions).Assembly.Location; //TODO" C:\\work\\new_gltf_exporter\\gltf-collision-detection\\GS.Gltf.Collision.Tests\\bin\\Debug\\netcoreapp3.1\\GS.Gltf.Collision.Tests.dll"
 
         string testRootPath = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).ToString();
 
@@ -18,6 +19,8 @@ namespace GS.Gltf.Collision.Tests
             {
                 Path.Combine(testRootPath,"Resources","box1","box1.gltf"),
                 Path.Combine(testRootPath,"Resources","box2","box2.gltf"),
+                Path.Combine(testRootPath,"Resources","box3","box3.gltf"),
+                Path.Combine(testRootPath,"Resources","box4","box4.gltf"),
             };
 
             var settings = new CollisionSettings(inputfiles)
@@ -29,9 +32,7 @@ namespace GS.Gltf.Collision.Tests
 
             var detector = new CollisionDetector(settings);
             var detectResult = detector.Detect();
-            detectResult = detectResult.Where(x => x.Collisions.Count > 0).ToList();
-
-
+            
             Assert.Pass();
         }
 
@@ -41,7 +42,7 @@ namespace GS.Gltf.Collision.Tests
 
             List<string> inputfiles = new List<string>()
             {
-                Path.Combine(testRootPath,"Resources","all_boxes2","all_boxes.gltf"),
+                Path.Combine(testRootPath,"Resources","BigModel","Hockey Arena.gltf"),
             };
 
             var settings = new CollisionSettings(inputfiles)
@@ -53,19 +54,6 @@ namespace GS.Gltf.Collision.Tests
 
             var detector = new CollisionDetector(settings);
             var detectResult = detector.Detect();
-            
-
-            // TODO Add elements to the file from inside the CollisionDetector depending on the property HiglightCollisions.
-            //var root = new GltfReader(inputfiles).RawModels[0];
-            //var testModel = detector.Models[0];
-            //foreach (var collision in testModel.InterModelCollisions)
-            //{
-            //    root.AddCollisionBBNode(collision.MinIntersectionBoundaries);
-            //}
-
-            //root.SaveGLTF(Path.Combine("C:", "Resources", "all_boxes", "collision.gltf"));
-            //root.SaveGLB(Path.Combine("C:", "Resources", "all_boxes", "test.glb"));
-
 
             Assert.Pass();
         }

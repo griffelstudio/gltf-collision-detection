1da55a29df01e2d9631adfef73c16c29202e4c56
Alexanderexe <cronergy100@mail.ru>
2021-07-27 21:39:14 +0300

    add demo project

---
+++ profiling/Program.cs
@@ -0,0 +1,35 @@
+﻿using GS.Gltf.Collision;
+using GS.Gltf.Collision.Tests;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace profiling
+{
+    class Program
+    {
+
+        private static readonly string path = typeof(TestCollisions).Assembly.Location;
+        private static readonly string path2 = Directory.GetParent(Directory.GetParent(Directory.GetParent(path).ToString()).ToString()).ToString();
+        private static readonly string path3 = Directory.GetParent(Directory.GetParent(path2).ToString()).ToString();
+        static void Main(string[] args)
+        {
+
+            
+        List<string> inputfiles = new List<string>()
+            {
+                Path.Combine(path3,"GS.Gltf.Collision.Tests","Resources","cutted_arena","Hockey Arena.gltf"),
+            };
+
+            var settings = new CollisionSettings(inputfiles)
+            {
+                InModelDetection = true,
+                Delta = 5f,
+                HiglightCollisions = CollisionHighlighing.MergeAll
+            };
+
+            var detector = new CollisionDetector(settings);
+            var detectResult = detector.Detect();
+        }
+    }
+}
